c38ebae4e38e8764a09620460aa8b8896f478bec
Nehal Chauhan <98140974+mRcOol7@users.noreply.github.com>
2024-12-08 23:49:58 +0530

    Update config.js (#5)

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // Firebase configuration
 window.firebaseConfig = {
-    apiKey: "AIzaSyBo9MQw6nMVk0jF3r9zGeRbAJ98SoTLmgc",
-    authDomain: "mechroad-6063c.firebaseapp.com",
-    databaseURL: "https://mechroad-6063c-default-rtdb.firebaseio.com/",
-    projectId: "mechroad-6063c",
-    storageBucket: "mechroad-6063c.appspot.com",
-    messagingSenderId: "758909410801",
-    appId: "1:758909410801:web:f0c9f1961575f7fffa3850",
-    measurementId: "G-ZNLVWBBN13"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
