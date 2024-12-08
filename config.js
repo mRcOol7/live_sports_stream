36a6d2a759560dba90a866f736b523ae92861478
mRcOol7 <196450307014@kjpbharuch.gujgov.edu.in>
2024-12-08 23:42:14 +0530

    .s

--- config.js
+++ config.js
@@ -1,14 +1,11 @@
-// Load environment variables from .env file
-require('dotenv').config();
-
-// Export Firebase config
+// Firebase configuration
 window.firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    apiKey: "AIzaSyBo9MQw6nMVk0jF3r9zGeRbAJ98SoTLmgc",
+    authDomain: "mechroad-6063c.firebaseapp.com",
+    databaseURL: "https://mechroad-6063c-default-rtdb.firebaseio.com/",
+    projectId: "mechroad-6063c",
+    storageBucket: "mechroad-6063c.appspot.com",
+    messagingSenderId: "758909410801",
+    appId: "1:758909410801:web:f0c9f1961575f7fffa3850",
+    measurementId: "G-ZNLVWBBN13"
 };
